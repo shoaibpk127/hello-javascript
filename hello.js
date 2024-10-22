0c1489bc35b0871a6595ff4df345bab8e02aa0ff
shoaibpk127 <shoaibpk127@gmail.com>
2024-10-22 21:23:48 +0600

    first commit

---
+++ hello.js
@@ -0,0 +1,34 @@
+// var num1 = 70;
+// var num2 = 60;
+// var total = num1 %num2;
+// console.log(total)
+
+// var num =-29;
+// var sum = Math.abs(num)
+// console.log(sum)
+
+// var voter = 0.5;
+// var sum = Math.round(voter);
+// console.log(sum)
+
+// var sum =4.09;
+// var num =Math.ceil(sum);
+// console.log(num)
+
+// var num =8.810;
+// var sum =Math.floor(num);
+// console.log(sum)
+
+// var lotary = Math.random() * 100;
+// console.log(lotary);
+
+// var a = 2;
+// var b = 5;
+// if(a<b){
+//     console.log("coumputer er matha khik nay")
+// }
+
+var a = 3;
+if(a<10){
+    console.log("computer er mata thik ace");
+}
